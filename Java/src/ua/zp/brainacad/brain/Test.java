3a909a211d51d2289110dcd465dc016273bdcaa8
shapovalalexandr1 <shapovalalexandr1@gmail.com>
2018-10-18 13:59:51 +0300

    commit from IDEA 18.10.2018

---
+++ Java/src/ua/zp/brainacad/brain/Test.java
@@ -0,0 +1,10 @@
+package ua.zp.brainacad.brain;
+
+/**
+ * Created by a.shapoval on 16.10.2018.
+ */
+public class Test {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+}
