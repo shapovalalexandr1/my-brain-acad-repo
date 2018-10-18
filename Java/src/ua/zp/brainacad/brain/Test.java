8483df22f867a2a61e8cd3c6cd07d861e2721b19
shapovalalexandr1 <shapovalalexandr1@gmail.com>
2018-10-18 16:27:03 +0300

    commit from IDEA 18.10.2018/16:26

--- Java/src/ua/zp/brainacad/brain/Test.java
+++ Java/src/ua/zp/brainacad/brain/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[]args){
         System.out.println("Hello World!");
         System.out.println("Test commit");
+        System.out.println("Test commit # 2");
     }
 }
