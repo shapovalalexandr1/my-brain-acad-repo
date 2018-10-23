89128c4e80dfc60559186915ae21934a5bb07c8f
evgentn <evgentitovn@gmail.com>
2018-10-23 18:33:58 +0300

    lab2 classroom1

--- Java/src/ua/zp/brainacad/brain/lab2/Main.java
+++ Java/src/ua/zp/brainacad/brain/lab2/Main.java
@@ -9,7 +9,8 @@ public class Main {
 
         int a = 4;
         int b = 4;
-
+        Double d = 5.5;
+        Double c = 4.5;
         System.out.println("Create instance of IntegralCalculator");
         IntegralCalculator calculator = new IntegralCalculator();
 
@@ -17,28 +18,48 @@ public class Main {
         BooleanCalculator booleanCalculator = new BooleanCalculator();
 
 
+        System.out.println("Create instance of DoubleCalculator");
+        DoubleCalculator doubleCalculator = new DoubleCalculator();
+
         System.out.println("\nTry calc int sum:");
         int sum = calculator.sum(a, b);
         ResultPrinter.print(a, "+", b, sum);
         System.out.println('\n');
 
-        //TODO call sub
+        System.out.println("\nTry calc Double sum:");
+        Double dsum = doubleCalculator.sum(d, c);
+        ResultPrinter.print(d, "+", c, dsum);
+        System.out.println('\n');
+
+        //TODO call sub +++++++++++++++++++
+            System.out.println("\nTry calc int sub:");
+            int sub = calculator.sub(a,b);
+            ResultPrinter.print(a, "-", b, sub);
+            System.out.println('\n');
 
         System.out.println("Try calc int multiply:");
         int multiply = calculator.mul(a, b);
         ResultPrinter.print(a, "*", b, multiply);
         System.out.println('\n');
 
-        //TODO call div
+        //TODO call div++++++++++++++++++++
+        System.out.println("\nTry calc int div:");
+        int div =calculator.div(a,b);
+        ResultPrinter.print(a, "/", b, div);
+        System.out.println('\n');
 
         a = 32;
         b = 2;
         System.out.println("Try calc boolean <<:");
         ResultPrinter.printBinary(a,"<<", b, booleanCalculator.shiftLeft(a, b));
-        booleanCalculator.shiftRight(2, 4);
+      //  booleanCalculator.shiftLeft(2, 4);
         System.out.println('\n');
 
         //TODO call >>
+        System.out.println("Try calc boolean >>:");
+        ResultPrinter.printBinary(a,">>", b, booleanCalculator.shiftRight(a, b));
+       // booleanCalculator.shiftRight(2, 4);
+        System.out.println('\n');
 
         b = 8;
         System.out.println("Try calc boolean XOR:");
@@ -46,5 +67,9 @@ public class Main {
         System.out.println('\n');
 
         //TODO call inverse
+        System.out.println("Try calc boolean inverse:");
+        int inv = booleanCalculator.inverse(a);
+        ResultPrinter.printBinary(a,"~",booleanCalculator.inverse(a));
+        System.out.println('\n');
     }
 }
